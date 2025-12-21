952ceb7e5cb1e367d5b590fb2b24d7a28d9dfc5b
bbrtj <bbrtj.pro@gmail.com>
2025-12-21 20:58:11 +0100

    Initial

---
+++ cpanfile
@@ -0,0 +1,8 @@
+on 'test' => sub {
+	requires 'Test2::V1' => '0';
+};
+
+requires 'perl' => '5.040';
+requires 'Mooish::Base' => '1.001';
+requires 'Gears' => '0';
+
---
+++ lib/Thunderhorse.pm
@@ -0,0 +1,44 @@
+package Thunderhorse;
+
+##################################
+# ~~~~~~~~~~~~ Ride ~~~~~~~~~~~~ #
+# ~~~~~~~~~~~ Thunder ~~~~~~~~~~ #
+# ~~~~~~~~ Thunderhorse ~~~~~~~~ #
+# ~~~~~~~~~~~ Revenge ~~~~~~~~~~ #
+##################################
+
+use v5.40;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Thunderhorse - A no-compromises brutally-good web framework
+
+=head1 SYNOPSIS
+
+	use Thunderhorse;
+
+	# do something
+
+=head1 DESCRIPTION
+
+This module lets you blah blah blah.
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2025 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
