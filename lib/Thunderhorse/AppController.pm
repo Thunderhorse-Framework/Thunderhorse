4adb8ce5c4d62909a558baae48d4dec6e755e138
bbrtj <bbrtj.pro@gmail.com>
2026-01-11 19:08:09 +0100

    Make sure methods returned by autoloaded "can" work well

--- lib/Thunderhorse/AppController.pm
+++ lib/Thunderhorse/AppController.pm
@@ -19,8 +19,11 @@ sub _run_method ($self, $method, @args)
 
 sub _can_method ($self, $method)
 {
-	return $self->app->can($method)
-		// $self->SUPER::_can_method($method);
+	if (my $can = $self->app->can($method)) {
+		return sub { shift->app->$can(@_) };
+	}
+
+	return $self->SUPER::_can_method($method);
 }
 
 __END__
